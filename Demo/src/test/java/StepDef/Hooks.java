cfa4fbc269ddb9e638cda279c0b2331a8433886c
Sevvelkaran <2k21it50@kiot.ac.in>
2025-04-07 10:07:49 +0530

    commit

--- Demo/src/test/java/StepDef/Hooks.java
+++ Demo/src/test/java/StepDef/Hooks.java
@@ -1,16 +1,18 @@
 package StepDef;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import Util.HelperClass;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class Hooks {
-	@Before
+	@BeforeMethod
 	public static void setUp() {
 		HelperClass.setUpDriver();
 	}
-	@After
+	@AfterMethod
 	public static void teardown(Scenario scenario) {
 		HelperClass.teardown();
 	}
